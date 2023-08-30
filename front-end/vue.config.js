459a1dd2991e3103d709cd9b5bdcd5be1d13d0fd
Gabriel Rodrigues <82984651+gbrods@users.noreply.github.com>
2023-08-30 00:16:10 -0300

    Update vue.config.js

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: ''
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
 })
