ea796d53205d5baba547457463bdeb88435e1460
Gabriel Rodrigues da Silva <gabriel.rodrigues129@poli.ufrj.br>
2023-08-30 00:39:56 -0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-deploy/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/butique_aluguel_de_roupas/" : "/",
 })
