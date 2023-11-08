18516f08b0bbaf9a2d9ad6c902d6f689cdc7a141
Felipe Villela <felipeavillela@gmail.com>
2023-11-07 22:49:18 -0300

    Pequenas mudanças de texto + diagrama de caso de uso

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -41,6 +41,12 @@
     <i class="fas fa-external-link-alt"></i> Acessar Planilha
   </a>
 </section>
+<section class="section use-case-diagram">
+  <h2><i class="fa-solid fa-table-columns"></i> Diagrama de Caso de Uso</h2>
+  <p>{{ textCaseUseDiagram }}</p>
+  <img :src="imagePathUseCaseDiagram" alt="Diagrama de Caso de Uso" style="max-width: 100%; height: auto;">
+
+</section>
 
     </section>
     <footer>
@@ -55,14 +61,15 @@ export default {
   data() {
     return {
       pageTitle: 'Butique de Aluguel de Roupas',
-      projectDescription: "Nosso projeto se concentra em criar uma plataforma online de fácil navegação, onde os usuários poderão visualizar uma ampla variedade de roupas de festa disponíveis para aluguel. Cada peça será apresentada com informações detalhadas, incluindo tamanho, cor, estilo e disponibilidade. O processo de reserva será simples, permitindo que os clientes selecionem suas escolhas e escolham a data de retirada.",
+      projectDescription: "Nosso projeto se concentra em criar uma plataforma online de fácil navegação, onde os usuários poderão visualizar uma ampla variedade de roupas de festa disponíveis para aluguel. Cada peça será apresentada com informações detalhadas, incluindo tamanho, cor, estilo e disponibilidade. O processo de reserva será simples, permitindo que os clientes selecionem suas escolhas e definam a data de retirada.",
       projectOverview: "Além disso, nosso sistema proporcionará benefícios tanto para os clientes quanto para a loja. Os clientes poderão acessar seus perfis, visualizar históricos de aluguel anteriores e receber notificações sobre novas adições ao catálogo. Para a loja, o sistema oferecerá um painel de gerenciamento de estoque, permitindo um controle preciso das peças disponíveis e suas respectivas datas de aluguel. Isso resultará em uma operação mais eficiente e uma experiência de cliente aprimorada.",
       projectImpact: "Ao implementar esse sistema de aluguel de roupas online, espera-se que a loja de roupas otimize suas operações, evite perdas por falta ou excesso de estoque, conflitos de datas e reservas. Além disso, espera-se que o atendimento aos clientes seja mais eficiente e ágil.",
       projectValue: "A proposta de valor é economizar tempo para os clientes. Com uma plataforma intuitiva, os usuários poderão visualizar facilmente um catálogo diversificado de roupas de festa disponíveis para aluguel. Isso permite que eles encontrem a peça perfeita de forma conveniente, economizando tempo e esforço na busca por roupas para eventos especiais. Além disso, o sistema oferecerá um processo de reserva simplificado, permitindo que os clientes assegurem suas escolhas com antecedência, minimizando conflitos de datas e garantindo que tenham a roupa ideal para seus momentos especiais.",
       teamMembers: "Membros do grupo: Karen Pacheco, Mikaela Rikberg, Felipe Villela e Gabriel Rodrigues",
-      softwareProject:"Nossa equipe construiu o Software Project Canvas para o projeto, que está disponível abaixo.",
-      imagePath: require('@/assets/software-project-canvas.png')
-
+      softwareProject:"Nossa equipe construiu o Software Project Canvas para o projeto, que está disponível abaixo:",
+      imagePath: require('@/assets/software-project-canvas.png'),
+      textCaseUseDiagram: "Nossa equipe construiu o Diagrama de Caso de Uso para o projeto, que está disponível abaixo:",
+      imagePathUseCaseDiagram: require('@/assets/use-case-diagram.png'),
     };
   }
 };
